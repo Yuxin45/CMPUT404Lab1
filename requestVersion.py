e0329d44961da1fa0840e9e92ac23517887eb586
claire45 <yliu17@ualberta.ca>
2021-01-15 16:02:23 -0700

    udpate:

--- requestVersion.py
+++ requestVersion.py
@@ -4,3 +4,6 @@ print(requests.__version__)
 
 r = requests.get('http://google.com/', auth=('user', 'pass'))
 print(r.status_code)
+
+githublink = requests.get("https://raw.githubusercontent.com/Yuxin45/CMPUT404Lab1/master/requestVersion.py")
+print(githublink.text)
